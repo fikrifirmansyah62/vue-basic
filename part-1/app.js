28fcbdfc9dc995beec5a0dae0ac31963f351be2b
M Fikri Firmansyah <fikrifirmansyahm62@gmail.com>
2023-10-31 13:08:13 +0700

    styling dengan vue

--- part-1/app.js
+++ part-1/app.js
@@ -6,6 +6,8 @@ var app = new Vue({
     cart: [],
     style: {
       label: ['font-weight-bold', 'mr-2'],
+      inputWidth: 60,
+      sliderStatus: false,
     },
   },
   mounted: function () {
@@ -14,6 +16,11 @@ var app = new Vue({
       .then((data) => {
         this.products = data;
       });
+  },
+  computed: {
+    sliderState: function () {
+      return this.style.sliderStatus ? 'd-flex' : 'd-none';
+    },
   },
   methods: {
     addItem: function (product) {
