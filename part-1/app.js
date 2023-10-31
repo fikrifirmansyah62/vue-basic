d53b11d865a55cd91580eb41420a02f1db1850c3
M Fikri Firmansyah <fikrifirmansyahm62@gmail.com>
2023-10-31 12:59:37 +0700

    Binding attribute array dan object

--- part-1/app.js
+++ part-1/app.js
@@ -4,6 +4,9 @@ var app = new Vue({
     maximum: 50,
     products: null,
     cart: [],
+    style: {
+      label: ['font-weight-bold', 'mr-2'],
+    },
   },
   mounted: function () {
     fetch('https://hplussport.com/api/products/order/price')
