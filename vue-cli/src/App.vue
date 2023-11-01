75db5d836d0781d1271b5e40802238b83abd438a
M Fikri Firmansyah <fikrifirmansyahm62@gmail.com>
2023-11-01 11:50:15 +0700

    ...

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <h1>CDShop</h1>
+    <p class="animate fadeInRight">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+      molestias in animi qui odit blanditiis deleniti reprehenderit temporibus!
+      Eligendi nostrum molestias nobis. Doloremque consequuntur, exercitationem
+      nemo corrupti error amet repellendus!
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FontAwesomeIcon,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
