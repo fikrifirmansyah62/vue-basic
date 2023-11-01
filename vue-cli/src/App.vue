a359a2b38cf2d1d040e06dafa002ff6002bb4533
M Fikri Firmansyah <fikrifirmansyahm62@gmail.com>
2023-11-01 13:04:25 +0700

    membuat component price

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -5,16 +5,19 @@
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda soluta consequuntur tempore saepe placeat eos possimus quisquam et enim? Quis velit esse nihil, et culpa eaque at? Quisquam, saepe culpa.
     </p>
     <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <price :value="4.23"></price>
   </div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Price from './components/Price.vue';
 
 export default {
   name: 'App',
   components: {
     FontAwesomeIcon,
+    Price,
   },
 };
 </script>
