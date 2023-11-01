2e2fbcbb157b47311bb2cb6ee7567125e0f82564
M Fikri Firmansyah <fikrifirmansyahm62@gmail.com>
2023-11-01 12:53:54 +0700

    install ulang vue

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>CDShop</h1>
+    <p class="animated fadeInRight">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda soluta consequuntur tempore saepe placeat eos possimus quisquam et enim? Quis velit esse nihil, et culpa eaque at? Quisquam, saepe culpa.
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+export default {
+  name: 'App',
+  components: {
+    FontAwesomeIcon,
+  },
+};
+</script>
